39c03e516677a6f3e087d98f60e00de306574a75
hakiem526 <hakiemm@hotmail.com>
2021-08-31 23:11:59 +0800

    Remove PriceImpactHandler

--- src/utility/PriceImpactHandler.py
+++
@@ -1,7 +0,0 @@
-class PriceImpactHandler:
-
-    def __init__(self, priceImpact):
-        self.priceImpact = priceImpact
-
-    def __str__(self):
-        return f'Price impact: {self.priceImpact}'
