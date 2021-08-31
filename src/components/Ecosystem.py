7a5c68246bb509ce6934dd47010d3810a353de73
hakiem526 <hakiemm@hotmail.com>
2021-08-31 23:10:57 +0800

    Added stub for run()

--- src/components/Ecosystem.py
+++ src/components/Ecosystem.py
@@ -1,4 +1,7 @@
 from .Dex import Dex
+from .MarketTaker import MarketTaker
+from typing import List
+import random
 
 # Parameters to set:
 #   1. AMM parameters
@@ -7,9 +10,27 @@ from .Dex import Dex
 #   2. MarketTaker parameters
 #       a. Probabiliies for buy TKN, insure TKN, claim insurance, buy INSR, sell INSR
 
+# Note: Must handle exceptions from actors
+
 # This class presets the behaviour of actors and handles interactions between them
 class Ecosystem:
-    def __init__(self, dex, marketTakers):
-        self.dex = Dex(dex)
+    def __init__(self, dex: Dex, marketTakers: List[MarketTaker]):
+        self.dex = dex
         self.marketTakers = marketTakers
 
+    # This function calls transactions
+    def run(self, numTransactions):
+        actions = ['buyInsr', 'sellInsr']
+        for i in range(numTransactions):
+            action = random.choice(actions)
+            actor = random.choice(self.marketTakers)
+            print(actor)
+            if action == 'buy':
+                # if(self.__checksForBuyInsr()):
+                pass
+            elif action == 'sell':
+                # print('sell!')
+                pass
+    
+    def __checksForBuyInsr(self):
+        pass
