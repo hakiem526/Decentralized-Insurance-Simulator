c2be189265cfa611cfd3fdfff5da3b70f3fe5b4b
hakiem526 <hakiemm@hotmail.com>
2021-08-31 23:11:35 +0800

    Init actors in simulation

--- Simulator.py
+++ Simulator.py
@@ -2,10 +2,18 @@ from src.components.Ecosystem import Ecosystem
 from src.components.Dex import Dex
 from src.components.MarketTaker import MarketTaker
 
+# init Dex
 dex = Dex(40, 100000000)
-x = MarketTaker(1)
-print(dex)
-print(x)
-x.buyInsr(dex, 0.1)
-print(dex)
-print(x)
+
+# init MarketTakers
+currId = 0
+marketTakers = []
+for i in range (10):
+    marketTakers.append(MarketTaker(currId, 10))
+    currId += 1
+
+# init Ecosystem
+ecosystem = Ecosystem(dex, marketTakers)
+
+# run sim
+ecosystem.run(5)
